5eefc6cfdda93b47ff30f8bf8d0b0c5be8c3e029
Kazi Minhaj <kaziminhajulhaider@gmail.com>
2021-04-05 09:56:13 +0600

    fresh push

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,10 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAsihSADFnF_FoZ6je8p1W9lilyqX7cHWQ",
-    authDomain: "burj-al-arab.firebaseapp.com",
-    databaseURL: "https://burj-al-arab.firebaseio.com",
-    projectId: "burj-al-arab",
-    storageBucket: "burj-al-arab.appspot.com",
-    messagingSenderId: "244522202387",
-    appId: "1:244522202387:web:f3801d0588387140e1d6e6"
-  };
-  export default firebaseConfig;
+  apiKey: "AIzaSyDLIN4O81LrM83t3MS4b8GbgVHPCXtQNLA",
+  authDomain: "burj-al-arab-67d57.firebaseapp.com",
+  projectId: "burj-al-arab-67d57",
+  storageBucket: "burj-al-arab-67d57.appspot.com",
+  messagingSenderId: "787240386293",
+  appId: "1:787240386293:web:c3057488a9125440f60fbc"
+};
+export default firebaseConfig;
